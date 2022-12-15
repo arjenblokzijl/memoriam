8339e926405d6fd93b7b6878adcff169ff3e27f5
Nadia Weijers <nadiaweijers@DESKTOP-9PP2FHV.localdomain>
2022-12-15 16:32:40 +0100

    Fixes styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,19 @@ module.exports = {
   content: ["./pages/**/*.js", "./components/**/*.js"],
   theme: {
     fontFamily: {
-      domine: 'Domine,serif',
-      montserrat500: 'Montserrat,sans-serif'
+      domine: "Domine,serif",
+      montserrat: "Montserrat,sans-serif",
     },
     extend: {
       colors: {
-        'brown': {
-          100: '#DFD8CF',
-          200: '#624A24'
-        }
-      }
+        brown: {
+          80: "#F5F5F5",
+          90: "#8F8780",
+          100: "#DFD8CF",
+          200: "#624A24",
+        },
+        yellow: "#DB6F0C",
+      },
     },
   },
   plugins: [],
